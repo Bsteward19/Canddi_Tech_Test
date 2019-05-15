07fc696d8c7855191603ada1b07687bcf2610330
Bsteward19 <50661404+Bsteward19@users.noreply.github.com>
2019-05-16 00:11:44 +0100

    Working code
    This is the working code. Only currently outputs previews, and doesn't save log to a file as of yet. Website must be manually inputted into file to be analysed.

---
+++ Test_04.js
@@ -0,0 +1,40 @@
+//load modules
+var Knwl = require("C:/Users/brend/.node_modules_global/node_modules/knwl.js");
+var request = require('C:/Users/brend/.node_modules_global/node_modules/request');
+var cheerios = require('C:/Users/brend/.node_modules_global/node_modules/cheerio');
+//create global variable to hold HTML information
+global.globalLog = "This is a variable";
+//print base variable contents to ensure its initialised
+console.log(globalLog);
+var knwlInstance = new Knwl('english');
+//load knwl modules such as dates ect.
+knwlInstance.register('dates', require('C:/Users/brend/.node_modules_global/node_modules/knwl.js/default_plugins/dates'));
+//get website, and return error if it cant be found. Put website here
+request('https://www.canddi.com/', function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+   //log would usually show HTML code, left blank for clarity
+    console.log(' ');
+  //put html into globalLog variable
+    globalLog = html;
+  //initialise globalLog into knwl to extract relevant information
+   knwlInstance.init(globalLog);
+  //get important information as specified in brief
+	var dates = knwlInstance.get('dates');
+	var phone = knwlInstance.get('phones');
+	var links = knwlInstance.get('links');
+	var emails = knwlInstance.get('emails');
+	var places = knwlInstance.get('places');
+	knwlInstance.get('dates');
+	knwlInstance.get('phones');
+	knwlInstance.get('links');
+	knwlInstance.get('emails');
+	knwlInstance.get('places');
+	console.log(dates);
+	console.log(phone);
+	console.log(links);
+	console.log(emails);
+	console.log(places);
+  }
+});
+
+
